61f71a91673e989e6e5c7c16c1079d612dcb372a
andrey <mastercatpro.tm@gmail.com>
2022-05-28 00:39:35 +0300

    add sign in form

---
+++ src/pages/Authorization/Authorization.tsx
@@ -0,0 +1,5 @@
+import { Page } from 'components/Layout/Page'
+
+export const Authorization = () => {
+  return <Page>123</Page>
+}
---
+++ src/pages/Authorization/components/SignIn.tsx
@@ -0,0 +1,12 @@
+import { ModalHeader } from '@chakra-ui/react'
+import { Text } from 'components/Text'
+
+export const SignIn = () => {
+  return (
+    <>
+      <ModalHeader>
+        <Text translation='' />
+      </ModalHeader>
+    </>
+  )
+}
